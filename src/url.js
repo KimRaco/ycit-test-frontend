256eea7149cffec271ed354ec54e0b4c12ccc91f
Kim Ramirez <kim.rc.1066@gmail.com>
2023-11-27 00:42:01 -0600

    url updated

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
 //const API_URL='http://localhost:8080'
-const API_URL='https://yocontigo-backend-imywoer5p-kimraco.vercel.app'
+const API_URL='http://yocontigo-backend-imywoer5p-kimraco.vercel.app'
 
 
 export { API_URL }
